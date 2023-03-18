eed62b535aabf6f975a925c2dec60e065c62edef
gbaidwan <116042346+gbaidwan@users.noreply.github.com>
2023-03-18 17:18:04 -0400

    fixed shi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+/*import "./App.css";
 import Home from "./pages/Courses";
 import Login from "./pages/Login";
 import Syllabus from "./pages/Upload";
@@ -21,3 +21,52 @@ function App() {
 }
 
 export default App;
+*/
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './components/logincomponent'
+import SignUp from './components/signupcomponent'
+import ForgotPassword from './components/ForgotPassword'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              Breaking-Binary
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
+}
+export default App
