d42fe69fba3bb930e57a816556844ada7b5bf817
elyknehc <kchen727@uwo.ca>
2023-03-22 17:05:20 -0400

    map with database

--- src/components/CourseItem.js
+++ src/components/CourseItem.js
@@ -3,19 +3,18 @@ import "../styles/Courses.css";
 import { useNavigate } from "react-router-dom";
 
 function CourseItem({ name, image, profname, id }) {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    console.log("This is a test")
-    return (
-        <div
-            className="courseItem"
-            onClick={() => {
-                navigate("/course/" + id);
-            }}
-        >
-            <h1 className="text">{name} </h1>
-        </div>
-    );
+	return (
+		<div
+			className="courseItem"
+			onClick={() => {
+				navigate("/course/" + id);
+			}}
+		>
+			<h1 className="text">{name} </h1>
+		</div>
+	);
 }
 
 export default CourseItem;
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,36 +1,36 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
-import {courseList} from "../courseList";
 import CourseItem from "../components/CourseItem";
 import Navbar from "../components/Navbar";
 import "../styles/Home.css";
 
 function Home() {
-    const [course_list, set_course_list] = useState([]);
+	const [course_list, setCourseList] = useState([]);
 
-    Axios.get('http://localhost:4000/api/courses', {})
-        .then(function (response) {
-            console.log(response.data);
-        })
+	useEffect(() => {
+		Axios.get("http://localhost:4000/api/courses").then((res) => {
+			setCourseList(res.data);
+			console.log(course_list);
+		});
+	}, []);
 
-    return (
-        <div>
-            <Navbar/>
-            <div className="courseList">
-                {courseList.map((course, index) => {
-                    return (
-                        <CourseItem
-                            className="courseList"
-                            name={course.name}
-                            image={course.image}
-                            profname={course.profname}
-                            id={index}
-                        />
-                    );
-                })}
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<Navbar />
+			<div className="courseList">
+				{course_list.map((course, index) => {
+					return (
+						<CourseItem
+							className="courseList"
+							name={course.name}
+							profname={course.profName}
+							key={index}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
