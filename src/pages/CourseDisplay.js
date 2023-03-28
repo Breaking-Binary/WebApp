65be954401215197f7023a8c1c390391c79a8ac0
elyknehc <kchen727@uwo.ca>
2023-03-28 13:17:48 -0400

    added some css

--- src/pages/CourseDisplay.js
+++ src/pages/CourseDisplay.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "../styles/Home.css";
 import Navbar from "../components/Navbar";
+import "../styles/CourseDisplay.css";
 
 function CourseDisplay() {
 	const [course_list, setCourseList] = useState([]);
@@ -20,40 +21,42 @@ function CourseDisplay() {
 	return (
 		<div>
 			<Navbar />
-			{course_list
-				.filter((courseElement, index) => index == id)
-				.map((currentCourse) => {
-					return (
-						<div>
-							<div> Professor's Name: {currentCourse.profName}</div>
-							<div> Professor's Email: {currentCourse.profEmail}</div>
+			<div className="info">
+				{course_list
+					.filter((courseElement, index) => index == id)
+					.map((currentCourse) => {
+						return (
+							<div>
+								<div> Professor's Name: {currentCourse.profName}</div>
+								<div> Professor's Email: {currentCourse.profEmail}</div>
 
-							{currentCourse.commitments.map((commitment) => {
-								return (
-									<div>
-										<div> Type of Class: {commitment.commitmentType} </div>
-										<div> Recurring Times: {commitment.dayOfWeek} </div>
-										<div> Duration: {commitment.duration} </div>
-										<div> Time: {commitment.time} </div>
-									</div>
-								);
-							})}
-							{currentCourse.evaluations.map((evaluation, index) => {
-								return (
-									<div>
-										<span> Evaluation {index + 1}: </span>
-										<div> Name: {evaluation.evaluationName} </div>
-										<div> Weight: {evaluation.weight} </div>
-										<div> Grade: {evaluation.grade} </div>
-										<div> Due Date: {evaluation.dueDate} </div>
-									</div>
-								);
-							})}
-							{/* <div> Assignments: {currentCourse.evaluations[0].grade} </div> */}
-							<div> Course Identifier: {currentCourse._id}</div>
-						</div>
-					);
-				})}
+								{currentCourse.commitments.map((commitment) => {
+									return (
+										<div>
+											<div> Type of Class: {commitment.commitmentType} </div>
+											<div> Recurring Times: {commitment.dayOfWeek} </div>
+											<div> Duration: {commitment.duration} </div>
+											<div> Time: {commitment.time} </div>
+										</div>
+									);
+								})}
+								{currentCourse.evaluations.map((evaluation, index) => {
+									return (
+										<div>
+											<span> Evaluation {index + 1}: </span>
+											<div> Name: {evaluation.evaluationName} </div>
+											<div> Weight: {evaluation.weight} </div>
+											<div> Grade: {evaluation.grade} </div>
+											<div> Due Date: {evaluation.dueDate} </div>
+										</div>
+									);
+								})}
+								{/* <div> Assignments: {currentCourse.evaluations[0].grade} </div> */}
+								<div> Course Identifier: {currentCourse._id}</div>
+							</div>
+						);
+					})}
+			</div>
 		</div>
 	);
 }
