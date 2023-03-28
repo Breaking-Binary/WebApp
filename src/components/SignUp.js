fee45f18de823b83b0ed3cb827febfd824e5a74f
gbaidwan <116042346+gbaidwan@users.noreply.github.com>
2023-03-28 16:58:30 -0400

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -25,114 +25,116 @@ export const SignUp1 = () => {
     }
     })
   };
-};
 
-return (
-  <div className="App">
-    <LoginNavBar />
-    <div className="auth-wrapper">
-      <div className="auth-inner">
-        <form onSubmit={this.handleSubmit}>
-          <h3>Sign Up</h3>
-          <div className="mb-3">
-            <form>
+  return (
+    <div className="App">
+      <LoginNavBar />
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <form onSubmit={this.handleSubmit}>
+            <h3>Sign Up</h3>
+            <div className="mb-3">
+              <form>
+                <label>
+                  First name
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={inputs.firstName || ""}
+                    onChange={handleChange}
+                  />
+                </label>
+              </form>
+            </div>
+            <div className="mb-3">
+              <form>
+                <label>
+                  Last name
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={inputs.lastName || ""}
+                    onChange={handleChange}
+                  />
+                </label>
+              </form>
+            </div>
+            <div className="mb-3">
+              <form>
+                <label>
+                  Email
+                  <input
+                    type="text"
+                    name="email"
+                    value={inputs.email || ""}
+                    onChange={handleChange}
+                  />
+                </label>
+              </form>
+            </div>
+            <div className="mb-3">
+              <form>
+                <label>
+                  Password
+                  <input
+                    type="text"
+                    name="password"
+                    value={inputs.password || ""}
+                    onChange={handleChange}
+                  />
+                </label>
+              </form>
+            </div>
+            <div className="mb-3">
               <label>
-                First name
+                Confirm Password
                 <input
                   type="text"
-                  name="firstName"
-                  value={inputs.firstName || ""}
+                  name="confirmPassword"
+                  value={inputs.confirmPassword || ""}
                   onChange={handleChange}
                 />
               </label>
-            </form>
-          </div>
-          <div className="mb-3">
-            <form>
-              <label>
-                Last name
-                <input
-                  type="text"
-                  name="lastName"
-                  value={inputs.lastName || ""}
-                  onChange={handleChange}
-                />
-              </label>
-            </form>
-          </div>
-          <div className="mb-3">
-            <form>
-              <label>
-                Email
-                <input
-                  type="text"
-                  name="email"
-                  value={inputs.email || ""}
-                  onChange={handleChange}
-                />
-              </label>
-            </form>
-          </div>
-          <div className="mb-3">
-            <form>
-              <label>
-                Password
-                <input
-                  type="text"
-                  name="password"
-                  value={inputs.password || ""}
-                  onChange={handleChange}
-                />
-              </label>
-            </form>
-          </div>
-          <div className="mb-3">
-            <label>
-              Confirm Password
-              <input
-                type="text"
-                name="confirmPassword"
-                value={inputs.confirmPassword || ""}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div className="mb-3">
-            <form>
-              <label>
-                school
-                <input
-                  type="text"
-                  name="school"
-                  value={inputs.school || ""}
-                  onChange={handleChange}
-                />
-              </label>
-            </form>
-          </div>
-          <div className="mb-3">
-            <form>
-              <label>
-                Courses
-                <input
-                  type="text"
-                  name="courses"
-                  value={inputs.courses || ""}
-                  onChange={handleChange}
-                />
-              </label>
-            </form>
-          </div>
-          <div className="d-grid">
-            <button type="submit" className="btn btn-primary">
-              Sign Up
-            </button>
-          </div>
-          <p className="forgot-password text-right">
-            Already registered? Log in <a href="/log-in">here</a>
-          </p>
-        </form>
+            </div>
+            <div className="mb-3">
+              <form>
+                <label>
+                  school
+                  <input
+                    type="text"
+                    name="school"
+                    value={inputs.school || ""}
+                    onChange={handleChange}
+                  />
+                </label>
+              </form>
+            </div>
+            <div className="mb-3">
+              <form>
+                <label>
+                  Courses
+                  <input
+                    type="text"
+                    name="courses"
+                    value={inputs.courses || ""}
+                    onChange={handleChange}
+                  />
+                </label>
+              </form>
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Sign Up
+              </button>
+            </div>
+            <p className="forgot-password text-right">
+              Already registered? Log in <a href="/log-in">here</a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+
