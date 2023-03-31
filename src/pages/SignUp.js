b7c844eb38630bd7e2b3ab799c6b1677c7427279
samueljcatania <69449284+samueljcatania@users.noreply.github.com>
2023-03-31 19:11:08 -0400

    Merged Andy's API Fix

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,7 +3,15 @@ import axios from "axios";
 import LoginNavBar from "../components/LoginNavBar";
 
 export const SignUp = () => {
-    const [inputs, setInputs] = useState([]);
+    const [inputs, setInputs] = useState({
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        school: "",
+        courses: [],
+    });
 
     const handleChange = (event) => {
         const name = event.target.name;
@@ -14,17 +22,18 @@ export const SignUp = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         alert(inputs);
-        axios.post(`${process.env.BACKEND_SERVER_URL}/api/users/`, {
-            params: {
-                // firstName,
-                // lastName,
-                // email,
-                // password,
-                // confirmPassword,
-                // school,
-                // courses
-            }
-        })
+        console.log(inputs);
+        axios
+            .post(`http://localhost:4000/api/users`, {
+                //   .post(`${process.env.BACKEND_SERVER_URL}/api/users/`, { (THIS DOESN"T WORK FOR SOME REASON)
+                firstname: inputs.firstname,
+                lastname: inputs.lastname,
+                username: inputs.email,
+                password: inputs.password,
+                school: inputs.school,
+            })
+            .then((r) => {
+            });
     };
 
     return (
@@ -32,108 +41,83 @@ export const SignUp = () => {
             <LoginNavBar/>
             <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <div className="container-lg mt-3">
-                        <form className="row g-3 needs-validation" onSubmit={handleSubmit} noValidate>
-                            <h3>Sign Up</h3>
-                            <div className="form-floating mb-3">
-                                <input className="form-control"
-                                       type="text"
-                                       name="email"
-                                       id="first_name"
-                                       placeholder="Enter first name"
-                                       value={email}
-                                       pattern="[a-zA-Z0-9!#$%&amp;'*+\/=?^_`{|}~.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+"
-                                       onChange={handleInputChange}
-                                       required/>
-                                <label className="form-label" htmlFor="first_name">First Name</label>
-                                <div className="invalid-feedback">Not a valid email.</div>
-                            </div>
-                            <div className="mb-3">
-                                <form>
-                                    <label>
-                                        Last name
-                                        <input
-                                            type="text"
-                                            name="lastName"
-                                            value={inputs.lastName || ""}
-                                            onChange={handleChange}
-                                        />
-                                    </label>
-                                </form>
-                            </div>
-                            <div className="mb-3">
-                                <form>
-                                    <label>
-                                        Email
-                                        <input
-                                            type="text"
-                                            name="email"
-                                            value={inputs.email || ""}
-                                            onChange={handleChange}
-                                        />
-                                    </label>
-                                </form>
-                            </div>
-                            <div className="mb-3">
-                                <form>
-                                    <label>
-                                        Password
-                                        <input
-                                            type="text"
-                                            name="password"
-                                            value={inputs.password || ""}
-                                            onChange={handleChange}
-                                        />
-                                    </label>
-                                </form>
-                            </div>
-                            <div className="mb-3">
-                                <label>
-                                    Confirm Password
-                                    <input
-                                        type="text"
-                                        name="confirmPassword"
-                                        value={inputs.confirmPassword || ""}
-                                        onChange={handleChange}
-                                    />
-                                </label>
-                            </div>
-                            <div className="mb-3">
-                                <form>
-                                    <label>
-                                        school
-                                        <input
-                                            type="text"
-                                            name="school"
-                                            value={inputs.school || ""}
-                                            onChange={handleChange}
-                                        />
-                                    </label>
-                                </form>
-                            </div>
-                            <div className="mb-3">
-                                <form>
-                                    <label>
-                                        Courses
-                                        <input
-                                            type="text"
-                                            name="courses"
-                                            value={inputs.courses || ""}
-                                            onChange={handleChange}
-                                        />
-                                    </label>
-                                </form>
-                            </div>
-                            <div className="d-grid">
-                                <button type="submit" className="btn btn-primary">
-                                    Sign Up
-                                </button>
-                            </div>
-                            <p className="forgot-password text-right">
-                                Already registered? Log in <a href="/log-in">here</a>
-                            </p>
-                        </form>
-                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <h3>Sign Up</h3>
+                        <div className="mb-3">
+                            <label>
+                                First name
+                                <input
+                                    type="text"
+                                    name="firstname"
+                                    value={inputs.firstname || ""}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
+                        <div className="mb-3">
+                            <label>
+                                Last name
+                                <input
+                                    type="text"
+                                    name="lastname"
+                                    value={inputs.lastname || ""}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
+                        <div className="mb-3">
+                            <label>
+                                Email
+                                <input
+                                    type="text"
+                                    name="email"
+                                    value={inputs.email || ""}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
+                        <div className="mb-3">
+                            <label>
+                                Password
+                                <input
+                                    type="text"
+                                    name="password"
+                                    value={inputs.password || ""}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
+                        <div className="mb-3">
+                            <label>
+                                Confirm Password
+                                <input
+                                    type="text"
+                                    name="confirmPassword"
+                                    value={inputs.confirmPassword || ""}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
+                        <div className="mb-3">
+                            <label>
+                                school
+                                <input
+                                    type="text"
+                                    name="school"
+                                    value={inputs.school || ""}
+                                    onChange={handleChange}
+                                />
+                            </label>
+                        </div>
+                        <div className="d-grid">
+                            <button type="submit" className="btn btn-primary">
+                                Sign Up
+                            </button>
+                        </div>
+                        <p className="forgot-password text-right">
+                            Already registered? Log in <a href="/log-in">here</a>
+                        </p>
+                    </form>
                 </div>
             </div>
         </div>
